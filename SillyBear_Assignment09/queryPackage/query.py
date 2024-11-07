87268e1a114c9a072650176b4b07a266b7edb27a
willgpa <wgpadgett@gmail.com>
2024-11-06 19:35:14 -0500

    Added queryPackage and query.py

---
+++ SillyBear_Assignment09/queryPackage/query.py
@@ -0,0 +1,49 @@
+import pyodbc
+from dataPackage.data import Data_loader
+import random
+
+
+class Queries:
+    def __init__(self):
+            self.conn = Data_loader()
+            self.cursor = self.conn.cursor()
+
+    def get_all_products(self):
+        """Fetch all products."""
+        product_query = "SELECT ProductID, [UPC-A ], Description, ManufacturerID, BrandID FROM tProduct"
+        self.cursor.execute(product_query)
+        return self.cursor.fetchall()
+
+    def get_manufacturer_name(self, manufacturer_id):
+        """Get manufacturer name."""
+        manufacturer_query = f"SELECT Manufacturer FROM tManufacturer WHERE ManufacturerID = {manufacturer_id}"
+        self.cursor.execute(manufacturer_query)
+        result = self.cursor.fetchone()
+        return result[0] if result else "Unknown Manufacturer"
+
+    def get_brand_name(self, brand_id):
+        """Get brand name."""
+        brand_query = f"SELECT Brand FROM tBrand WHERE BrandID = {brand_id}"
+        self.cursor.execute(brand_query)
+        result = self.cursor.fetchone()
+        return result[0] if result else "Unknown Brand"
+
+    def get_number_of_items_sold(self, product_id):
+        """Get number of items sold."""
+        sales_query = f"""
+        SELECT TOP (100) PERCENT SUM(dbo.tTransactionDetail.QtyOfProduct) AS NumberOfItemsSold
+        FROM dbo.tTransactionDetail 
+        INNER JOIN dbo.tTransaction 
+        ON dbo.tTransactionDetail.TransactionID = dbo.tTransaction.TransactionID 
+        WHERE dbo.tTransaction.TransactionTypeID = 1 
+        AND dbo.tTransactionDetail.ProductID = {product_id}
+        """
+        self.cursor.execute(sales_query)
+        result = self.cursor.fetchone()
+        return result[0] if result and result[0] is not None else 0
+
+    def close_connection(self):
+        """Close connection."""
+        self.cursor.close()
+        self.conn.close()
+
