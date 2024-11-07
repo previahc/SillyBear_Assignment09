9bee4b07ac7cd53dac3370c05165119b568a379b
ap1805 <aryanjpatel2003@gmail.com>
2024-11-06 19:50:21 -0500

    Added mainPackage and main.py with to invoke query functionality and print required output

---
+++ SillyBear_Assignment09/mainPackage/main.py
@@ -0,0 +1,28 @@
+
+import pyodbc
+import random
+from queryPackage.query import Queries
+if __name__ == "__main__":
+    # Initialize the ProductAnalytics class
+    Queries = Queries()
+
+    products = Queries.get_all_products()
+
+    selected_product = random.choice(products)
+    description = selected_product.Description
+
+    product_id = selected_product.ProductID
+    manufacturer_id = selected_product.ManufacturerID
+    brand_id = selected_product.BrandID
+
+    manufacturer = Queries.get_manufacturer_name(manufacturer_id)
+
+    brand = Queries.get_brand_name(brand_id)
+ 
+   
+    number_of_items_sold = Queries.get_number_of_items_sold(product_id)
+ 
+   
+    output = f"The product '{description}', manufactured by {manufacturer} and branded as {brand}, has sold {number_of_items_sold} items."
+    print(output)
+
